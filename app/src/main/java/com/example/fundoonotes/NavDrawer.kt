1ae789d7ab0d3e77041afc512699d0c1eac20023
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2025-03-12 11:57:18 +0530

    Nav drawer integrated

--- app/src/main/java/com/example/fundoonotes/NavDrawer.kt
+++ app/src/main/java/com/example/fundoonotes/NavDrawer.kt
@@ -38,7 +38,7 @@ class NavDrawer : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_notes, R.id.nav_reminders, R.id.nav_create_label
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
