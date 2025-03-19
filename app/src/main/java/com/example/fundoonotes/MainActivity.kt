07e9431e7e4714991520e526328ec0f4bc67092d
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2025-03-19 15:10:05 +0530

    Toast message displayed for each nav

--- app/src/main/java/com/example/fundoonotes/MainActivity.kt
+++ app/src/main/java/com/example/fundoonotes/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.fundoonotes
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,45 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.main)
+        val drawerButton: ImageButton = findViewById(R.id.drawer_button)
+
+        drawerButton.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        navView.setNavigationItemSelectedListener { item: MenuItem ->
+            val itemId: Int = item.itemId
+
+            when (itemId) {
+                R.id.navNotes -> {
+                    Toast.makeText(this, "Notes selected", Toast.LENGTH_SHORT).show()
+                }
+                R.id.navReminders -> {
+                    Toast.makeText(this, "Reminders selected", Toast.LENGTH_SHORT).show()
+                }
+                R.id.navLabels -> {
+                    Toast.makeText(this, "Create/Edit labels selected", Toast.LENGTH_SHORT).show()
+                }
+                R.id.navArchive -> {
+                    Toast.makeText(this, "Archive selected", Toast.LENGTH_SHORT).show()
+                }
+                R.id.navBin -> {
+                    Toast.makeText(this, "Bin selected", Toast.LENGTH_SHORT).show()
+                }
+                R.id.navSettings -> {
+                    Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT).show()
+                }
+                R.id.navFeedback -> {
+                    Toast.makeText(this, "Send app feedback selected", Toast.LENGTH_SHORT).show()
+                }
+                R.id.navHelp -> {
+                    Toast.makeText(this, "Help selected", Toast.LENGTH_SHORT).show()
+                }
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
         }
     }
 }
