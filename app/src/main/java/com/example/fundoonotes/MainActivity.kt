55fc3459e22ebdfa15f45ef3b9aea961ccbccd21
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2025-03-20 18:43:06 +0530

    Added dynmically changing header icons

--- app/src/main/java/com/example/fundoonotes/MainActivity.kt
+++ app/src/main/java/com/example/fundoonotes/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.fundoonotes
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -27,6 +29,11 @@ class MainActivity : AppCompatActivity() {
     private var currentNavItemId: Int = R.id.navNotes // Default selected item
 
     private lateinit var title_text: TextView
+    private lateinit var search_icon: ImageView
+    private lateinit var layout_toggle_icon: ImageView
+    private lateinit var profile_icon: ImageView
+    private lateinit var header_options: ImageView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,6 +58,13 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = findViewById(R.id.main)
         val drawerButton: ImageButton = findViewById(R.id.drawer_button)
         title_text = findViewById(R.id.tvHeaderTitle)
+        layout_toggle_icon = findViewById(R.id.layout_toggle_icon)
+        search_icon = findViewById(R.id.search_icon)
+        profile_icon = findViewById(R.id.profile_icon)
+        header_options = findViewById(R.id.header_options)
+        search_icon.visibility = View.GONE
+        header_options.visibility = View.GONE
+
 
         drawerButton.setOnClickListener {
             drawerLayout.openDrawer(GravityCompat.START)
@@ -69,23 +83,42 @@ class MainActivity : AppCompatActivity() {
                     // Navigate to NotesFragment
                     val fragment = NoteFragment()
                     title_text.text = getString(R.string.notes)
+
+                    layout_toggle_icon.visibility = View.VISIBLE
+                    profile_icon.visibility = View.VISIBLE
+                    header_options.visibility = View.GONE
+                    search_icon.visibility = View.GONE
+
                     loadFragment(fragment)
                 }
                 R.id.navReminders -> {
                     // Navigate to RemindersFragment
                     val fragment = RemindersFragment()
                     title_text.text = getString(R.string.reminders)
+                    layout_toggle_icon.visibility = View.VISIBLE
+                    profile_icon.visibility = View.GONE
+                    header_options.visibility = View.GONE
+                    search_icon.visibility = View.VISIBLE
                     loadFragment(fragment)
                 }
                 R.id.navLabels -> {
                     // Navigate to LabelsFragment
                     val fragment = LabelsFragment()
+//                    layout_toggle_icon.visibility = View.GONE
+                    layout_toggle_icon.visibility = View.GONE
+                    profile_icon.visibility = View.GONE
+                    header_options.visibility = View.GONE
+                    search_icon.visibility = View.GONE
                     title_text.text = getString(R.string.create_labels)
                     loadFragment(fragment)
                 }
                 R.id.navArchive -> {
                     // Navigate to ArchiveFragment
                     val fragment = ArchiveFragment()
+                    layout_toggle_icon.visibility = View.VISIBLE
+                    profile_icon.visibility = View.GONE
+                    header_options.visibility = View.GONE
+                    search_icon.visibility = View.VISIBLE
                     title_text.text = getString(R.string.archive)
                     loadFragment(fragment)
                 }
@@ -93,6 +126,10 @@ class MainActivity : AppCompatActivity() {
                     // Navigate to BinFragment
                     val fragment = BinFragment()
                     title_text.text = getString(R.string.bin)
+                    layout_toggle_icon.visibility = View.VISIBLE
+                    profile_icon.visibility = View.GONE
+                    header_options.visibility = View.GONE
+                    search_icon.visibility = View.GONE
                     loadFragment(fragment)
                 }
                 // Handle other menu items...
