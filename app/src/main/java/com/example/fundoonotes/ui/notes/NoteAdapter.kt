2f046dfa53d01d00314eb6bded7f35854582abee
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2025-03-22 10:21:41 +0530

    Added rounded bg for Alarm and label name

--- app/src/main/java/com/example/fundoonotes/ui/notes/NoteAdapter.kt
+++ app/src/main/java/com/example/fundoonotes/ui/notes/NoteAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.fundoonotes.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fundoonotes.R
 import com.example.fundoonotes.data.model.Note
@@ -28,6 +30,7 @@ class NoteAdapter(
         val tvDescription: TextView = itemView.findViewById(R.id.tvDescription)
         val llReminder: LinearLayout = itemView.findViewById(R.id.llReminder)
         val tvTimeDate: TextView = itemView.findViewById(R.id.tvTimeDate)
+        val tvLabelName: TextView = itemView.findViewById(R.id.tvLabelName)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
@@ -50,6 +53,15 @@ class NoteAdapter(
         } else {
             holder.llReminder.visibility = View.GONE
         }
+// Handle Displaying Label Name
+            if (note.labels.isNotEmpty()) {
+                holder.tvLabelName.visibility = View.VISIBLE
+                holder.tvLabelName.text = note.labels.joinToString(", ")
+            }
+            else{
+                holder.tvLabelName.visibility = View.GONE
+            }
+
 
         // Set click listeners
         holder.itemView.setOnClickListener {
