de013321be3333a0676e23bd7894b7415df329b4
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2025-03-27 10:48:16 +0530

    Google Auth Dependencies installed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
 
         // ... other dependencies ...
 
-        implementation("com.google.firebase:firebase-analytics:21.5.0")
+    implementation("com.google.firebase:firebase-analytics:21.5.0")
 
     // Import the BoM for the Firebase platform
     implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
@@ -63,5 +63,13 @@ dependencies {
 
         // ... more dependencies ...
     implementation(libs.firebase.analytics)
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+
+    // Also add the dependencies for the Credential Manager libraries and specify their versions
+    implementation("androidx.credentials:credentials:1.3.0")
+    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
 
 }
