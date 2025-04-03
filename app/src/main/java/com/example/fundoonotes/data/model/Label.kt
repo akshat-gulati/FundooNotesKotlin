991f042b206d07f655af7084f87bdd7b31f2aff3
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2025-04-03 21:16:19 +0530

    Data class edited to keep noteIds and userId

--- app/src/main/java/com/example/fundoonotes/data/model/Label.kt
+++ app/src/main/java/com/example/fundoonotes/data/model/Label.kt
@@ -3,5 +3,6 @@ package com.example.fundoonotes.data.model
 data class Label(
     val id: String = "",
     val name: String = "",
-    val noteIds: HashSet<String>
+    val noteIds: List<String> = listOf<String>(),
+    val userId: String = ""
 )
