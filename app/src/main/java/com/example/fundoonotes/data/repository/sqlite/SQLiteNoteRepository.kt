14218e4b8e121f95bd3b7cb03e18526d73a7668d
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2025-04-11 11:04:30 +0530

    Prepared sqlite Table structure for notes

--- app/src/main/java/com/example/fundoonotes/data/repository/sqlite/SQLiteNoteRepository.kt
+++ app/src/main/java/com/example/fundoonotes/data/repository/sqlite/SQLiteNoteRepository.kt
@@ -1,6 +1,8 @@
 package com.example.fundoonotes.data.repository.sqlite
 
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
 import com.example.fundoonotes.data.model.Note
 import com.example.fundoonotes.data.repository.interfaces.NotesInterface
@@ -8,12 +10,53 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class SQLiteNoteRepository(private val context: Context): NotesInterface {
+class SQLiteNoteRepository(context: Context): NotesInterface, SQLiteOpenHelper(context,
+    DATABASE_NAME, null,
+    DATABASE_VERSION
+) {
     private val _notesState = MutableStateFlow<List<Note>>(emptyList())
     val notesState: StateFlow<List<Note>> = _notesState.asStateFlow()
 
     companion object{
         private const val TAG = "SQLiteRepository"
+        private const val DATABASE_NAME = "notesapp.db"
+        private const val DATABASE_VERSION = 1
+        private const val TABLE_NAME = "all_notes"
+
+        private const val COLUMN_ID = "id"
+        private const val COLUMN_TITLE = "title"
+        private const val COLUMN_DESCRIPTION = "description"
+        private const val COLUMN_TIMESTAMP = "timestamp"
+        private const val COLUMN_LABELS = "labels"
+        private const val COLUMN_DELETED = "deleted"
+        private const val COLUMN_ARCHIVED = "archived"
+        private const val COLUMN_REMINDER_TIME = "reminderTime"
+        private const val COLUMN_DELETED_TIME = "deletedTime"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val createTableQuery = """
+        CREATE TABLE $TABLE_NAME (
+            $COLUMN_ID TEXT PRIMARY KEY,
+            $COLUMN_TITLE TEXT,
+            $COLUMN_DESCRIPTION TEXT,
+            $COLUMN_TIMESTAMP INTEGER,
+            $COLUMN_LABELS TEXT,
+            $COLUMN_DELETED INTEGER,
+            $COLUMN_ARCHIVED INTEGER
+        )
+    """.trimIndent()
+        db?.execSQL(createTableQuery)
+    }
+
+    override fun onUpgrade(
+        db: SQLiteDatabase?,
+        oldVersion: Int,
+        newVersion: Int
+    ) {
+        val dropTableQuery = "DROP TABLE IF EXISTS $TABLE_NAME"
+        db?.execSQL(dropTableQuery)
+        onCreate(db)
     }
 
     override fun fetchNoteById(noteId: String, onSuccess: (Note) -> Unit) {
@@ -22,7 +65,7 @@ class SQLiteNoteRepository(private val context: Context): NotesInterface {
     }
 
     override fun fetchNotes() {
-        Log.d(TAG, "fetchNotes not yet implemented")
+
     }
 
     override fun addNewNote(title: String, description: String, reminderTime: Long?): String {
@@ -37,4 +80,6 @@ class SQLiteNoteRepository(private val context: Context): NotesInterface {
     override fun deleteNote(noteId: String) {
         Log.d(TAG, "deleteNote not yet implemented")
     }
+
+
 }
