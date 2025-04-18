92bd046348a54d27f9198f39c3a035c17be24420
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2025-04-18 13:28:07 +0530

    NoteEditViewModel file created

---
+++ app/src/main/java/com/example/fundoonotes/ui/noteEdit/NoteEditViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.fundoonotes.ui.noteEdit
+
+import com.example.fundoonotes.data.repository.NoteLabelRepository
+import com.example.fundoonotes.data.repository.dataBridge.LabelDataBridge
+import com.example.fundoonotes.data.repository.dataBridge.NotesDataBridge
+
+class NoteEditViewModel {
+
+    private lateinit var notesDataBridge: NotesDataBridge
+    private lateinit var labelDataBridge: LabelDataBridge
+    private lateinit var noteLabelRepository: NoteLabelRepository
+
+
+
+}
